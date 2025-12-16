3ff9ceb66754ab87f9f7b927119504bee0efe302
zahrae028 <fatimabk028@gmail.com>
2025-12-16 15:24:20 +0100

    Add sidebar layout component

---
+++ backend/includes/sidebar.php
@@ -0,0 +1,8 @@
+
+<div class="sidebar">
+    <h2>Unity Care</h2>
+    <a href="/unity-care2/backend/dashboard/index.php">Dashboard</a>
+    <a href="/unity-care2/backend/patients/list.php">Patients</a>
+    <a href="/unity-care2/backend/doctors/list.php">Doctors</a>
+    <a href="/unity-care2/backend/departments/list.php">Departments</a>
+</div>
