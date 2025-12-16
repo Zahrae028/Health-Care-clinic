6a5886188cdc9a1a9c294c2aad4fd0d8b444b46f
zahrae028 <fatimabk028@gmail.com>
2025-12-16 15:25:48 +0100

    Add patient deletion functionality

---
+++ backend/patients/delete.php
@@ -0,0 +1,18 @@
+ <?php
+ require_once '../../config/database.php'; 
+
+ 
+
+ $conn = db_connect();
+
+ $id = $_GET['id'];
+        $stmt = mysqli_prepare(
+            $conn,
+            "DELETE FROM patients WHERE id = ?"
+        );
+
+        mysqli_stmt_bind_param($stmt,"i",$id);
+        mysqli_stmt_execute($stmt); 
+
+        echo "<script>location.href = 'list.php'</script>";
+?>
